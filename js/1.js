31e6bba489dbbe0c3d55722b5d094d6ad1b4c7d4
gwxcsny53 <gwxcsny53@163.com>
2022-10-18 10:58:21 +0800

    Update 1.js

--- js/1.js
+++ js/1.js
@@ -26,9 +26,7 @@ function checkTime(i) {
 }
 var cname = prompt("请输入你的名字", "名字");
 var mm = prompt("请输入你的学号", "学号");
-if (cname != "aaaa") {
-  x = document.getElementById("demo"); //查找元素
-  var a = cname + " " + mm;
-  x.innerHTML = a; //改变内容
-}
-//		document.write("<p style='color: red;'>"+name+"</p>")
+var a = cname + " " + mm;
+document.getElementById("demo").innerHTML = a; //改变内容
+
+
