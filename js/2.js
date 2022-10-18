c74df1d4966746c555983e75a4a19b46f9f8f4b7
gwxcsny53 <gwxcsny53@163.com>
2022-10-18 14:41:14 +0800

    Update 2.js

--- js/2.js
+++ js/2.js
@@ -28,6 +28,6 @@ var cname = prompt("请输入你的名字", "名字");
 var mm = prompt("请输入你的学号", "学号");
 
 ; //查找元素
-var a = "学生："+cname + " " + mm;
+var a = cname + " " + mm;
 document.getElementById("demo").innerHTML = a; //改变内容
 
